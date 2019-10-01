8e5f4c7e17eb59399066ed8a48fdb5129d1dda57
Cons19 <razvan1598@gmail.com>
2019-10-01 16:29:29 +0200

    Add high and low temperatures to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require("request");
 
+
+
 const forecast = (latitude, longitude, callback) => {
 
     const url = "https://api.darksky.net/forecast/b2f4623add06500cd0bc531e74d5b9fd/" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "?units=si";
@@ -10,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback("Unable to find location", undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The highest temperature today is ${body.daily.data[0].temperatureHigh}. The lowest temperature today is ${body.daily.data[0].temperatureLow}. There is a ${body.currently.precipProbability}% chance of rain.`);
         }
     });
 };
